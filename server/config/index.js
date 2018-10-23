be44c09becc488f9888c8f4f902ce2d2ca4c4705
Alexzanderk <kotsarev.a@gmail.com>
2018-10-23 22:39:17 +0300

    changed config

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,10 @@
-
 module.exports = {
-    version: '1.0.0',
-    port: process.env.PORT || 3001,
-    mongodbUri: process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/book-shelf' : process.env.MONGODB_URL,
-    secret: 'SuperSecret',
-    salt_i: 10
-}
+	version: '1.0.0',
+	port: process.env.PORT || 3001,
+	mongodbUri:
+		process.env.NODE_ENV === 'development'
+			? 'mongodb://localhost:27017/book-shelf'
+			: process.env.MONGODB_URI,
+	secret: process.env.SECRET,
+	salt_i: 10
+};
