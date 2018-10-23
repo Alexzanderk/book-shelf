f0ea7b29e09e4a705fb70c4f4016443831982488
Alexzanderk <kotsarev.a@gmail.com>
2018-10-23 23:04:44 +0300

    change config

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,7 @@
 module.exports = {
 	version: '1.0.0',
 	port: process.env.PORT || 3001,
-	mongodbUri:
-		process.env.NODE_ENV === 'development'
-			? 'mongodb://localhost:27017/book-shelf'
-			: process.env.MONGODB_URI,
+	mongodbUri: process.env.MONGODB_URI,
 	secret: process.env.SECRET,
 	salt_i: 10
 };
